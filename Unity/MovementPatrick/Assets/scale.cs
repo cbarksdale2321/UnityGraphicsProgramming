f133bf7315dab15589bb97427e59d182941a19b5
Charles Barksdale <charles.barksdale@loop.colum.edu>
2019-10-06 15:30:09 -0500

    Finished Movement

---
+++ Unity/MovementPatrick/Assets/scale.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scale : MonoBehaviour {
+    public float speed = 5f;
+
+    Vector3 temp;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        temp = transform.localScale;
+
+        temp.x += Time.deltaTime;
+
+        transform.localScale = temp;
+	}
+}
